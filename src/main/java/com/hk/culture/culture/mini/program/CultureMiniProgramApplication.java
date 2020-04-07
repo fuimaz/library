531103d4272d2bf72061c110cc11142b1ffdd477
jiman <fujiman331@gmail.com>
2020-04-07 23:13:53 +0800

    Initial commit

---
+++ src/main/java/com/hk/culture/culture/mini/program/CultureMiniProgramApplication.java
@@ -0,0 +1,13 @@
+package com.hk.culture.culture.mini.program;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CultureMiniProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CultureMiniProgramApplication.class, args);
+	}
+
+}
