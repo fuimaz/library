d57539e3e593f3a3cf286ce001ee5f77f5ed944f
jiman <fujiman331@gmail.com>
2020-08-24 20:45:21 +0800

    修改校验配置

--- src/main/java/com/hk/library/config/SecurityConfiguration.java
+++ src/main/java/com/hk/library/config/SecurityConfiguration.java
@@ -42,7 +42,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/libBook**").permitAll()
+                .antMatchers("/libBook/**").permitAll()
                 .antMatchers("/product/**").permitAll()
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest()
